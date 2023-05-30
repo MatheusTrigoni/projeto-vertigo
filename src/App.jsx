fa73daa47bd849a6f0e6b9144bd76a8558b36da6
MatheusTrigoni <matheusbr360@gmail.com>
2023-05-30 20:33:08 -0300

    Primeiro commit

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import Logo from './components/Logo'
+
+export function App() {
+    return (
+        <>
+            <Logo />
+        </>
+    )
+}
---
+++ src/components/Logo.jsx
@@ -0,0 +1,23 @@
+import logo from '../images/logo.png'
+import V from '../images/V.png'
+import e from '../images/e.png'
+import r from '../images/r.png'
+import t from '../images/t.png'
+import i from '../images/i.png'
+import g from '../images/g.png'
+import o from '../images/o.png'
+
+export function Logo() {
+    return (
+        <div>
+            <img src={logo} alt="imagem da logo" />
+            <img src={V} alt="V" />
+            <img src={e} alt="e" />
+            <img src={r} alt="r" />
+            <img src={t} alt="t" />
+            <img src={i} alt="i" />
+            <img src={g} alt="g" />
+            <img src={o} alt="o" />
+        </div>
+    )
+}
